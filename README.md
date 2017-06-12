992ac7e2f51ff50f73c4e2f1ad98aff46c5e114f
Matt McKegg <matt@wetsand.co.nz>
2017-06-13 01:07:00 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ssb-contacts
+
+[scuttlebot](http://scuttlebutt.nz/) plugin for getting reduced "contacts" state.
+
+Who follows who? Find out fast!
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,55 @@
+var FlumeReduce = require('flumeview-reduce')
+var ref = require('ssb-ref')
+
+exports.name = 'contacts'
+exports.version = require('./package.json').version
+exports.manifest = {
+  stream: 'source',
+  get: 'async'
+}
+
+exports.init = function (ssb, config) {
+  return ssb._flumeUse('contacts', FlumeReduce(0, reduce, map))
+}
+
+function reduce (result, item) {
+  if (!result) result = {}
+  if (item) {
+    for (var source in item) {
+      var valuesForSource = result[source] = result[source] || {}
+      for (var key in item[source]) {
+        var valuesForKey = valuesForSource[key] = valuesForSource[key] || {}
+        for (var dest in item[source][key]) {
+          var value = item[source][key][dest]
+          if (!valuesForKey[dest] || value[1] > valuesForKey[dest][1]) {
+            valuesForKey[dest] = value
+          }
+        }
+      }
+    }
+  }
+  return result
+}
+
+function map (msg) {
+  if (msg.value.content && msg.value.content.type === 'contact' && ref.isLink(msg.value.content.contact)) {
+    var source = msg.value.author
+    var dest = msg.value.content.contact
+    var values = {}
+
+    if ('following' in msg.value.content) {
+      values[source] = {
+        following: {
+          [dest]: [msg.value.content.following, msg.value.timestamp]
+        }
+      }
+      values[dest] = {
+        followers: {
+          [source]: [msg.value.content.following, msg.value.timestamp]
+        }
+      }
+    }
+
+    return values
+  }
+}
